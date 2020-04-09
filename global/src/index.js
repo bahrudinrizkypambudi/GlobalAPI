8f4fd981d70975acad83081bc2214ed7b7b61224
bahrudinrizkypambudi <bahrudinrizky313@gmail.com>
2020-04-09 16:32:39 +0700

    Update SRC

---
+++ global/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import HelloComponent from './component/HelloComponent';
+import StateFullComponent from './container/StateFullComponent';
+
+// function HelloWorld() {
+//   return <p>ini adalah function component</p>
+// }
+// const HelloWorld = () => {
+//   return <p> ini adalah arrow function </p>
+// }
+
+// class StateFullcomponent extends React.Component {
+//   render() {
+//     return <p> ini adalah StateFullcomponent </p>
+//   }
+// }
+
+import BlogPost from './container/BlogPost/BlogPost';
+
+ReactDOM.render(
+  <BlogPost />,
+  document.getElementById('content')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
